8bb9c5ed7a82e201e703316ded685621439efd5c
RaviGhaghada <ravighaghada28@gmail.com>
2020-06-18 09:11:40 +0100

    Added ArticleCreateView to backend

--- backend/djreact/articles/api/urls.py
+++ backend/djreact/articles/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ArticleDetailView, ArticleListView
+from .views import ArticleDetailView, ArticleListView, ArticleCreateView
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
+    path('create/', ArticleCreateView.as_view()),
     path('<pk>', ArticleDetailView.as_view())
+
 ]
