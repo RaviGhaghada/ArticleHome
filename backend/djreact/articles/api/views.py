49f17d71d2ccadd23b3551d4ceca6aa77a780b0c
RaviGhaghada <ravighaghada28@gmail.com>
2020-06-20 18:44:43 +0100

    Made Article List and Retrieve accessible to Any

--- backend/djreact/articles/api/views.py
+++ backend/djreact/articles/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from articles.models import Article
 from .serializers import ArticleSerializer
+
 
  
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -11,6 +12,15 @@ class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
 
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
 
 # from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
